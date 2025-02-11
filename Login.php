da0f63be4e3bd425d36a18727af235f83f8cb873
Eduarda.Castro <castroedu739@gmail.com>
2025-02-11 20:15:03 -0300

    pagina de login

--- Login.php
+++ Login.php
@@ -1,25 +1,45 @@
 <?php
- 
- if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+
+// Verificar se foi enviando dados via POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
-    $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
-    $celular = (isset($_POST["celular"]) && $_POST["celular"] != null) ? $_POST["celular"] : NULL;
-} else if (!isset($id)) {
+    $senha = (isset($_POST["senha"]) && $_POST["senha"] != null) ? $_POST["senha"] : "";
+    
+} else if (!isset($nome)) {
     // Se não se não foi setado nenhum valor para variável $id
-    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $nome = (isset($_GET["nome"]) && $_GET["nome"] != null) ? $_GET["nome"] : "";
     $nome = NULL;
-    $email = NULL;
-    $celular = NULL;
+    $senha = NULL;
+    
 }
 
 try {
-    $conexao = new PDO("pgsql:host=localhost; dbname=postgres", "postgres", "pabd");
-    echo "deu certo";
+    $conexao = new PDO("pgsql:host=localhost; dbname=Mukados", "postgres", "eduar26");
 } catch (PDOException $erro) {
     echo "Erro na conexão:" . $erro->getMessage();
 }
 
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM login WHERE usuario = :nome AND senha = :senha");
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $senha);
+        $stmt->execute();
+         
+        // Verifica se encontrou um usuário com as credenciais fornecidas
+        if ($stmt->rowCount() > 0) {
+            // Usuário autenticado com sucesso
+            $_SESSION['usuario'] = $nome;
+            header("Location: pagina_protegida.php"); // Redireciona para a página desejada
+            exit();
+        } else {
+            // Credenciais inválidas
+            $erro = "Usuário ou senha incorretos.";
+        }
+    } catch (PDOException $erro) {
+        echo "Erro na conexão: " . $erro->getMessage();
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -32,6 +52,12 @@ try {
         <form action="?act=save" method="POST" name="form1" >
           <h1>Agenda de contatos</h1>
           <hr>
+          <?php
+        // Exibe a mensagem de erro, se houver
+            if (isset($erro)) {
+                echo "<p style='color:red;'>$erro</p>";
+            }
+          ?>
           Nome: 
           <input type="text" name="nome" />
           Senha: 
